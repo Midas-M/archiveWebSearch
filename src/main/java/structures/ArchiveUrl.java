e0875931210967c3cecce1989594199a8c442ac2
Meladianos <p.meladianos@gmail.com>
2017-02-15 15:06:47 +0200

    init

--- src/main/java/structures/ArchiveUrl.java
+++ src/main/java/structures/ArchiveUrl.java
@@ -1,9 +1,15 @@
 package structures;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Created by midas on 2/15/2017.
  */
 public class ArchiveUrl {
     private String url;
-    private String dateFrom="";
+    private LocalDate dateFrom;
+    private LocalDate dateTO;
+    //DateTimeFormatter formatter = DateTimeFormatter.ISO_INSTANT;
+    //LocalDate dateFrom = LocalDate.parse(text, formatter);
 }
